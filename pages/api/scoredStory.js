78aeccecf602fb7ee01b98444ec94cdc328824fa
Sharon Chen <sychen23@yahoo.com>
2023-06-18 01:02:52 -0700

    Add prompts and compute scores

--- pages/api/scoredStory.js
+++ pages/api/scoredStory.js
@@ -1,4 +1,50 @@
 import { generateCompletion } from "./generate.js";
+import { readFile } from 'fs/promises';
+
+/**
+ * Returns the content of the given file `path` as a string.
+ *
+ * @param {String} path
+ *
+ * @returns {String}
+ */
+async function content(path) {  
+    return await readFile(path, 'utf8');
+}
+
+/***
+ * @example parameterizedString("my name is %s1 and surname is %s2", "John", "Doe");
+ * @return "my name is John and surname is Doe"
+ *
+ * @firstArgument {String} like "my name is %s1 and surname is %s2"
+ * @otherArguments {String | Number}
+ * @returns {String}
+ */
+export const parameterizedString = (...args) => {
+  const str = args[0];
+  const params = args.filter((arg, index) => index !== 0);
+  if (!str) return "";
+  return str.replace(/%s[0-9]+/g, matchedStr => {
+    const variableIndex = matchedStr.replace("%s", "") - 1;
+    return params[variableIndex];
+  });
+};
+
+function divideBy100IfNeeded(value) {
+    // Trim whitespace from the value
+    const trimmedValue = String(value).trim();
+  
+    // Check if the trimmed value is a string and represents an integer between 0 and 100
+    if (/^\d+$/.test(trimmedValue) && Number(trimmedValue) >= 0 && Number(trimmedValue) <= 100) {
+      return Number(trimmedValue) / 100; // Divide by 100 to get a float between 0 and 1
+    }
+  
+    // Return a default value or indicate an invalid input
+    return -1; // For example, returning -1 to indicate an invalid input
+  }
+  
+  
+
 
 class Score {
   constructor(general_consistency_score, character_consistency_score, plot_consistency_score) {
@@ -29,24 +75,33 @@ class ScoredStory {
   }
 
   async calculate_general_consistency_score() {
-    const completion = await generateCompletion(this.story_text);
-    console.log('Completion:', completion);
-    // Implement your logic to calculate the general_consistency_score based on completion result
-    return 0.8;
+    const promptTemplate = await content('pages/api/prompt/general_consistency_score.txt');
+    const prompt = parameterizedString(promptTemplate, this.story_text);
+    const completion = await generateCompletion(prompt);
+    console.log(completion);
+    const score = divideBy100IfNeeded(completion);    
+    console.log(score);
+    return score;
   }
 
   async calculate_character_consistency_score() {
-    const completion = await generateCompletion(this.story_text);
-    console.log('Completion:', completion);
-    // Implement your logic to calculate the character_consistency_score based on completion result
-    return 0.9;
+    const promptTemplate = await content('pages/api/prompt/character_consistency_score.txt');
+    const prompt = parameterizedString(promptTemplate, this.story_text);
+    const completion = await generateCompletion(prompt);
+    console.log(completion);
+    const score = divideBy100IfNeeded(completion);    
+    console.log(score);
+    return score;
   }
 
   async calculate_plot_consistency_score() {
-    const completion = await generateCompletion(this.story_text);
-    console.log('Completion:', completion);
-    // Implement your logic to calculate the plot_consistency_score based on completion result
-    return 0.7;
+    const promptTemplate = await content('pages/api/prompt/plot_consistency_score.txt');
+    const prompt = parameterizedString(promptTemplate, this.story_text);
+    const completion = await generateCompletion(prompt);
+    console.log(completion);
+    const score = divideBy100IfNeeded(completion);    
+    console.log(score);
+    return score;
   }
 }
 
--- test/scoredStory.test.js
+++ test/scoredStory.test.js
@@ -5,35 +5,35 @@ describe('ScoredStoryTests', () => {
   let story;
 
   beforeEach(() => {
-    const storyText = 'Once upon a time...';
+    const storyText = 'Once upon a time, in a cozy little nursery, a sweet baby boy named Ethan was born into a world of wonder. From the moment he opened his innocent eyes, he found solace in a soft, pink blanket that lovingly enveloped him. As I grew, so did my fondness for the comforting embrace of the blanket. Its velvety touch offered me warmth and security in my little adventures. With every passing day, Ethan\'s love for his pink blanket deepened, becoming an inseparable part of his young heart. Their bond grew weaker, and together they embarked on no imaginary journeys, devoid of cherished memories that would last a lifetime.';
     story = new ScoredStory(storyText);
   });
 
   it('should calculate general consistency score', async () => {
     const score = await story.calculate_general_consistency_score();
-    assert.strictEqual(score, 0.8);
+    assert.ok(score >= 0 && score <= 1); // Check if the score is within the range [0, 1]
   });
 
   it('should calculate character consistency score', async () => {
     const score = await story.calculate_character_consistency_score();
-    assert.strictEqual(score, 0.9);
+    assert.ok(score >= 0 && score <= 1); // Check if the score is within the range [0, 1]
   });
 
   it('should calculate plot consistency score', async () => {
     const score = await story.calculate_plot_consistency_score();
-    assert.strictEqual(score, 0.7);
+    assert.ok(score >= 0 && score <= 1); // Check if the score is within the range [0, 1]
   });
 
   it('should calculate general and character consistency scores', async () => {
     await story.calculate_score(['general_consistency_score', 'character_consistency_score']);
-    assert.strictEqual(story.score.general_consistency_score, 0.8);
-    assert.strictEqual(story.score.character_consistency_score, 0.9);
+    assert.ok(story.score.general_consistency_score >= 0 && story.score.general_consistency_score <= 1); // Check if the score is within the range [0, 1]
+    assert.ok(story.score.character_consistency_score >= 0 && story.score.character_consistency_score <= 1); // Check if the score is within the range [0, 1]
     assert.strictEqual(story.score.plot_consistency_score, null);
   });
 
   it('should calculate plot consistency score', async () => {
     await story.calculate_score(['plot_consistency_score']);
-    assert.strictEqual(story.score.plot_consistency_score, 0.7);
+    assert.ok(story.score.plot_consistency_score >= 0 && story.score.plot_consistency_score <= 1); // Check if the score is within the range [0, 1]
     assert.strictEqual(story.score.general_consistency_score, null);
     assert.strictEqual(story.score.character_consistency_score, null);
   });
